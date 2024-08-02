4cffbb7a4fdaf786e09363a8d1568de13987eebc
MarkusKonk <m.konkol@52north.org>
2024-08-02 14:55:45 +0200

    add mean_by_group function as R script

---
+++ mean_by_group.R
@@ -0,0 +1,36 @@
+##############################################################################################.
+## 3. mean_by_group ####
+## calculate data average per site, per year, per season and per HELCOM_ID ###################.
+## Can we use Datamash function for this in Galaxy workflows? ################################.
+## if we cannot - I will work more on this. ##################################################.
+## At the moment, quick and easy version, just to continue the data analysis##################.
+##############################################################################################.
+
+#RUN WITH
+#Rscript mean_by_group.R "data_out_peri_conv.csv" "data_out_seasonal_means.csv"
+
+args <- commandArgs(trailingOnly = TRUE)
+print(paste0('R Command line args: ', args))
+in_data_path = args[1]
+out_result_path = args[2]
+
+library(magrittr)
+library(dplyr)
+
+data_mean_by_group <- data.table::fread(in_data_path)
+
+
+data_mean_by_group$transparency_m <- as.numeric(data_mean_by_group$transparency_m)
+
+out_seasonal_means <- data_mean_by_group %>%
+  group_by(longitude, latitude, Year_adj_generated, group_labels, HELCOM_ID) %>%
+  summarise(Secchi_m_mean = mean(transparency_m)) %>%
+  ungroup() %>% 
+  group_by(Year_adj_generated, group_labels, HELCOM_ID) %>%
+  summarise(Secchi_m_mean_annual = mean(Secchi_m_mean)) %>%
+  ungroup()
+
+
+## Output: Now need to store output:
+print(paste0('Write result to csv file: ', out_result_path))
+data.table::fwrite(out_seasonal_means , file = out_result_path) 
