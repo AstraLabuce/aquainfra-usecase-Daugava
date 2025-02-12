dd436af26489d030cab203aeb0569f88c77366e0
MarkusKonk <m.konkol@52north.org>
2025-02-12 10:23:57 +0100

    rm packaes needed for map_static and map_shapefile

--- .binder/install.R
+++ .binder/install.R
@@ -3,26 +3,21 @@ if (!requireNamespace("remotes", quietly = TRUE)) {
 }
 
 required_packages_versions <- list(
-  "sf" = "1.0-14",
-  "magrittr" = "2.0.3",
-  "dplyr" = "1.1.2",
-  "janitor" = "2.2.0",
-  "sp" = "2.0-0",
-  "data.table" = "1.14.8",
-  "readxl" = "1.4.3",
-  "jsonlite" = "1.8.7",
-  "lubridate" = "1.9.3",
   "curl" = "5.2.1",
   "zoo" = "1.8-12",
+  "readxl" = "1.4.3",
   "tidyr" = "1.3.0",
   "Kendall" = "2.2.1",
   "ggplot2" = "3.5.1",
-  "viridis" = "0.6.4",
-  "mapview" = "2.11.2",
-  "webshot" = "0.5.5",
-  "tmap" = "3.3-3",
-  "tmaptools" = "3.1-1",
-  "rosm" = "0.3.0"
+  "jsonlite" = "1.8.7",
+  "dplyr" = "1.1.2",
+  "lubridate" = "1.9.3",
+  "sf" = "1.0-14",
+  "magrittr" = "2.0.3",
+  "janitor" = "2.2.0",
+  "sp" = "2.0-0",
+  "data.table" = "1.14.8",
+  "viridis" = "0.6.4"
 )
 
 install_if_missing <- function(pkg, version) {
