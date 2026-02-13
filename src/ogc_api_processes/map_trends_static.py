cd1606bd74b958d440b3c93989f3869b1883f156
Merret Buurman <merret.buurman@igb-berlin.de>
2026-02-13 18:08:50 +0000

    pygeoapi: Moved run_docker function into separate utils module.

--- src/ogc_api_processes/map_trends_static.py
+++ src/ogc_api_processes/map_trends_static.py
@@ -1,10 +1,12 @@
 import logging
-import subprocess
 import json
 import os
 import requests
-from urllib.parse import urlparse
 from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
+# daugava repo has hyphen in it, so we cannot import it in the normal python way:
+#from pygeoapi.process.aquainfra-usecase-Daugava.src.ogc_api_processes.docker_utils import run_docker_container
+import importlib
+docker_utils = importlib.import_module("pygeoapi.process.aquainfra-usecase-Daugava.src.ogc_api_processes.docker_utils")
 
 '''
 Output file name: map_trends_static-xyz.png
@@ -103,7 +105,7 @@ class MapTrendsStaticProcessor(BaseProcessor):
         ]
 
         # Run docker container
-        returncode, stdout, stderr = run_docker_container(
+        returncode, stdout, stderr = docker_utils.run_docker_container(
             self.docker_executable,
             self.image_name,
             self.script_name,
@@ -146,58 +148,3 @@ class MapTrendsStaticProcessor(BaseProcessor):
     def __repr__(self):
         return f'<MapTrendsStaticProcessor> {self.name}'
 
-
-def run_docker_container(
-        docker_executable,
-        image_name,
-        script_name,
-        output_dir,
-        job_id,
-        script_args
-    ):
-    LOGGER.debug('Will use this image: %s' % image_name)
-
-    # Create container name
-    # Note: Only [a-zA-Z0-9][a-zA-Z0-9_.-] are allowed
-    #container_name = "%s_%s" % (image_name.split(':')[0], os.urandom(5).hex())
-    container_name = "%s_%s" % (image_name.split(':')[0], job_id)
-    LOGGER.debug(f'Prepare running docker (image {image_name}, container: {container_name})')
-
-    # Define paths inside the container
-    container_out = '/out'
-
-    # Replace host out with container out:
-    sanitized_args = []
-    for arg in script_args:
-        if isinstance(arg, str) and output_dir is not None and output_dir in arg:
-            newarg = arg.replace(output_dir, container_out)
-        else:
-            # In any case, the newarg has to be a string:
-            newarg = str(arg)
-        sanitized_args.append(newarg)
-
-    # Assemble docker command:
-    docker_command = [
-        docker_executable, "run", "--rm", "--name", container_name,
-        "-v", f"{output_dir}:{container_out}",
-        "-e", f"R_SCRIPT={script_name}",  # Set the R_SCRIPT environment variable
-        image_name,
-        "--",  # Indicates the end of Docker's internal arguments and the start of the user's arguments
-    ]
-    docker_command = docker_command + sanitized_args
-
-
-    LOGGER.debug('Docker command: %s' % docker_command)
-    
-    # Run container
-    try:
-        LOGGER.debug('Start running docker container')
-        result = subprocess.run(docker_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout = result.stdout.decode()
-        stderr = result.stderr.decode()
-        LOGGER.debug('Finished running docker container')
-        return result.returncode, stdout, stderr
-
-    except subprocess.CalledProcessError as e:
-        LOGGER.debug('Failed running docker container')
-        return e.returncode, e.stdout.decode(), e.stderr.decode()
